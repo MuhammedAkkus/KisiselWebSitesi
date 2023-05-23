a8d0ecbadd031bf2c7f9f0056803bc7db61929c6
MuhammedAkkus <95915651+MuhammedAkkus@users.noreply.github.com>
2023-05-23 13:13:42 +0300

    Add files via upload
    Site oluşturulurken HTML5, CSS3, PHP ve JavaScript'e ek olarak artık MySQL ve Apache server kullanmaya başladım. XAMPP ile localde sunucumu çalıştırıyorum.

---
+++ MuhammedAkkus/index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <title>Anasayfa | Muhammed Akkuş</title>
+    <link rel="stylesheet" href="css/style.css">
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kalam&display=swap" rel="stylesheet">
+   
+    <script src="https://kit.fontawesome.com/f2ff860d39.js" crossorigin="anonymous"></script>
+    
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+<link rel="stylesheet" href="owl/owl.theme.default.min.css">
+
+</head>
+<body>
+    
+    <section id="menu">
+        <div id="logo">Muhammed Akkuş</div>
+        <nav>
+            <a href=""><i class="fa-solid fa-otter icon"></i>Anasayfa</a>
+            <a href=""><i class="fa-solid fa-user-astronaut icon"></i>Hakkımda</a>
+            <a href=""><i class="fa-solid fa-biohazard icon"></i>Eğitim ve Sertifakalar</a>
+            <a href=""><i class="fa-brands fa-space-awesome icon"></i>Faaliyetler</a>
+            <a href=""><i class="fa-solid fa-satellite icon"></i>İletişim</a>
+        </nav>
+    </section>
+    
+    <section id="banner">
+        <div id="black">
+            
+        </div>
+        
+        <div id="icerik">
+           <h2>Muhammed Akkuş</h2>
+           <hr width=300 align=left>
+           <p>Bu bir deneme yazısıdır. Yapacağım Web Sitesi son şeklini almadan resim olan ilgili yerlere çocukluk fotoğraflarımı koyarken yazılara da bilgilendirici bu notları ekleyeceğim.</p>
+            
+        </div>
+    </section>
+    
+    <sectioan id="hakkımda">
+        <h3>Hakkımda</h3>
+        <div class="container">
+            <div id="sol">
+                <h5 id="h5sol">Bu bir deneme yazısıdır. İlgili yazı gelinceye kadar... </h5>
+            </div>
+            <div id="sag">
+               <span>B</span>
+               <p id="psag">
+                   u bir deneme yazısıdır. Yapacağım Web Sitesi son şeklini almadan resim olan ilgili yerlere çocukluk ve sıradan ekran fotoğraflarımı koyarken yazılara da bilgilendirici bu notları ekleyeceğim.</p>
+                
+            </div>
+  
+        
+        <img src="img/denemeiki.png" alt=""
+        class="img-fluid mt100">
+        
+        <p id="pson">Bu bir deneme yazısıdır. Yapacağım Web Sitesi son şeklini almadan resim olan ilgili yerlere çocukluk ve sıradan ekran fotoğraflarımı koyarken yazılara da bilgilendirici bu notları ekleyeceğim.</p>
+        
+        </div>
+    </sectioan>
+    
+    <section id="egitimler">
+       <div class="container">
+           
+           <h3>Eğitimler</h3>
+       <div class="owl-carousel owl-theme">
+           
+           <div class="card item" data-merge=1.5>
+               <img src="img/deneme.jpg" alt="" class="img-fluid">
+               <h5 class="baslikcard">Proje Yönetimi 1</h5>
+               <p class="cardp">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+           </div>
+           
+           <div class="card item" data-merge=1.5>
+               <img src="img/deneme.jpg" alt="" class="img-fluid">
+               <h5 class="baslikcard">Proje Yönetimi 2</h5>
+               <p class="cardp">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+           </div>
+           
+              <div class="card item" data-merge=1.5>
+               <img src="img/deneme.jpg" alt="" class="img-fluid">
+               <h5 class="baslikcard">Proje Yönetimi 3</h5>
+               <p class="cardp">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+           </div>
+            
+           </div>
+           
+        </div>
+        
+    </section>
+    
+    <section id="faaliyetler">
+       <div class="container">
+           <h3 id="faaliyetlerh3">Faaliyetler</h3>
+           
+           <div class="sutun-sol-sag">
+               <img src="img/denemedort.png" alt="" class="img-fluid oval">
+               <h4 class="faaliyetlerisim">Google DSC</h4>
+               <p class="faal">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+               
+               <div class="faaliyet-ikon">
+                   <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                   <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                   <a href="#"><i class="fa-brands fa-google social"></i></a>
+               </div>
+               
+           </div>
+           
+           <div class="sutun">
+               <img src="img/denemedort.png" alt="" class="img-fluid oval">
+               <h4 class="faaliyetlerisim">BANÜ SET</h4>
+               <p class="faal">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+               
+               <div class="faaliyet-ikon">
+                   <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                   <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                   <a href="#"><i class="fa-brands fa-google social"></i></a>
+               </div>
+               
+           </div>
+           
+           <div class="sutun-sol-sag">
+               <img src="img/denemedort.png" alt="" class="img-fluid oval">
+               <h4 class="faaliyetlerisim">Google Akademisi</h4>
+               <p class="faal">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+               
+               <div class="faaliyet-ikon">
+                   <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                   <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                   <a href="#"><i class="fa-brands fa-google social"></i></a>
+               </div>
+               
+           </div>
+           
+       </div>
+        
+    </section>
+    
+    <section id="iletisim">
+        <div class="container">
+            <h3 id="h3iletisim">İletişim</h3>
+            
+            <form action="index.php" method="post">
+            
+            <div id="iletisimopak">
+                
+                <div id="formgroup">
+                    <div id="solform">
+                        <input type="text" name="isim" placeholder="Ad Soyad" required class="form-control">
+                        <input type="text" name="tel" placeholder="Telefon Numarası" required class="form-control">
+                    </div>
+                    <div id="sagform">
+                        <input type="email" name="mail" placeholder="Mail Adresiniz" required class="form-control">
+                        <input type="text" name="konu" placeholder="Konu Başlığı" required class="form-control">
+                    </div>
+                    
+                    <textarea name="mesaj" id="" cols="30" placeholder="Mesaj Giriniz" rows="10" required class="form-control"></textarea>
+                    
+                    <input type="submit" value="Gönder">
+                    </div>
+                <div id="adres">
+                    
+                    <h4 id="adresbaslik">Adres: </h4>
+                    <p class="adresp">Yeni Mah.</p>
+                    <p class="adresp">Şehit Tnk. Teğmen Doğuş Uran Caddesi</p>
+                    <p class="adresp">1165. Sokak NO:105/A</p>
+                    <p class="adresp">Bandırma/Balıkesir</p>
+                    <p class="adresp">0531 770 5059</p>
+                    <p class="adresp">Email: muhammedakkus.tr@gmail.com</p>
+                    
+                    
+                </div>
+                
+            </div>
+            </form>
+            <footer>
+                
+                <div id="copyright">2023 |
+           
+               Tüm hakları Saklıdır</div>
+               
+               <div id="socialfooter"> 
+               
+                <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram social"></i></a>
+                <a href="#"><i class="fa-brands fa-google social"></i></a>
+                
+                </div>
+                
+                <a href="#menu"><i class="fa-solid fa-jet-fighter-up" id="up"> Touch Me</i></a>
+                
+            </footer>
+            
+        </div>
+        </section>
+    
+    
+<script src="https://code.jquery.com/jquery-3.7.0.slim.min.js" integrity="sha256-tG5mcZUtJsZvyKAxYLVXrmjKBVLd6VpVccqz/r4ypFE=" crossorigin="anonymous"></script>    
+    
+<script src="owl/owl.carousel.min.js"></script>
+
+<script src="owl/script.js"></script>
+</body>
+</html>
+
+
+<?php
+
+include("baglanti.php");
+
+if(isset($_POST["isim"], $_POST["tel"], $_POST["mail"], $_POST["konu"], $_POST["mesaj"]))
+{
+    $adsoyad=$_POST["isim"];
+    $telefon=$_POST["tel"];
+    $email=$_POST["mail"];
+    $konu=$_POST["konu"];
+    $mesaj=$_POST["mesaj"];
+    
+    $ekle="INSERT INTO iletisim (adsoyad, telefon, mail, konu, mesaj) VALUES ('".$adsoyad."','".$telefon."','".$email."','".$konu."','".$mesaj."')";
+    
+    if($baglan->query($ekle)===TRUE)
+    {
+        echo "<script>alert('Mesajınız Başarı ile Gönderilmiştir.')</script>";
+    }
+    
+    else{
+        echo "<script>alert('Mesajınız Gönderilirken Bir Hata ile karşılaşılmıştır.')</script>";
+    }
+    
+}
+
+?>
