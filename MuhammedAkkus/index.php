36528218044813fdb50cfb3fea7eb2f12db062c1
MuhammedAkkus <95915651+MuhammedAkkus@users.noreply.github.com>
2023-05-24 17:31:14 +0300

    Add files via upload
    Kişisel Web Sitemin tamamen bitmiş V.1 sürümü

--- MuhammedAkkus/index.php
+++ MuhammedAkkus/index.php
@@ -20,11 +20,11 @@
     <section id="menu">
         <div id="logo">Muhammed Akkuş</div>
         <nav>
-            <a href=""><i class="fa-solid fa-otter icon"></i>Anasayfa</a>
-            <a href=""><i class="fa-solid fa-user-astronaut icon"></i>Hakkımda</a>
-            <a href=""><i class="fa-solid fa-biohazard icon"></i>Eğitim ve Sertifakalar</a>
-            <a href=""><i class="fa-brands fa-space-awesome icon"></i>Faaliyetler</a>
-            <a href=""><i class="fa-solid fa-satellite icon"></i>İletişim</a>
+            <a href="index.php"><i class="fa-solid fa-otter icon"></i>Anasayfa</a>
+            <a href="#hakkımda"><i class="fa-solid fa-user-astronaut icon"></i>Hakkımda</a>
+            <a href="#egitimler"><i class="fa-solid fa-biohazard icon"></i>Eğitim ve Sertifakalar</a>
+            <a href="#faaliyetler"><i class="fa-brands fa-space-awesome icon"></i>Faaliyetler</a>
+            <a href="#iletisim"><i class="fa-solid fa-satellite icon"></i>İletişim</a>
         </nav>
     </section>
     
@@ -36,7 +36,7 @@
         <div id="icerik">
            <h2>Muhammed Akkuş</h2>
            <hr width=300 align=left>
-           <p>Bu bir deneme yazısıdır. Yapacağım Web Sitesi son şeklini almadan resim olan ilgili yerlere çocukluk fotoğraflarımı koyarken yazılara da bilgilendirici bu notları ekleyeceğim.</p>
+           <p>Google Developer Student Clubs ile Sosyal Etkinlik ve Eğlence Topluluğunun eş zamanlı Proje Koordinatörlüğü yürüten Muhammed Akkuş (sol-arka dördüncü) ve Google Developer Student Clubs (siyah t-shirt) ile Sosyal Etkinlik ve Eğlence Topluluğu (beyaz t-shirt) ekiplerine ait bir fotoğraf...</p>
             
         </div>
     </section>
@@ -45,20 +45,19 @@
         <h3>Hakkımda</h3>
         <div class="container">
             <div id="sol">
-                <h5 id="h5sol">Bu bir deneme yazısıdır. İlgili yazı gelinceye kadar... </h5>
+                <h5 id="h5sol">Because the only thing that stays with you from the moment you're born until the moment you die is your dreams.</h5>
             </div>
             <div id="sag">
                <span>B</span>
                <p id="psag">
-                   u bir deneme yazısıdır. Yapacağım Web Sitesi son şeklini almadan resim olan ilgili yerlere çocukluk ve sıradan ekran fotoğraflarımı koyarken yazılara da bilgilendirici bu notları ekleyeceğim.</p>
-                
+                   andırma Onyedi Eylül Üniversitesi – Bilgisayar Mühendisliği bölümü üzerine eğitim hayatına devam etmektedir. Kendini geliştirmeyi seven, sıcakkanlı, çözüm odaklı, araştırmacı, girişken biridir. Veri Bilimi ve Makine Öğrenimi üzerine uzmanlaşmaktadır. Python, Matlab, MySQL, PowerBI en çok kullandığı yazılım enstrünmanlarıdır. Yazılımı bir amaçtan ziyade hayallerine somut bir biçim kazandırabildiği bir araç olarak görmektedir. 
             </div>
   
         
-        <img src="img/denemeiki.png" alt=""
+        <img src="img/hakkimda.jpg" alt=""
         class="img-fluid mt100">
         
-        <p id="pson">Bu bir deneme yazısıdır. Yapacağım Web Sitesi son şeklini almadan resim olan ilgili yerlere çocukluk ve sıradan ekran fotoğraflarımı koyarken yazılara da bilgilendirici bu notları ekleyeceğim.</p>
+        <p id="pson">Döneminin en başarılı etkinliklerinden biri olan "Techno Z" etkinliğini gerçekleştiren Bandırma Onyedi Eylül Üniversitesi Google Developer Student Clubs ekibi ve ekibin Proje Koordinatörü Muhammed Akkuş (sol-arka ikinci)</p>
         
         </div>
     </sectioan>
@@ -70,22 +69,41 @@
        <div class="owl-carousel owl-theme">
            
            <div class="card item" data-merge=1.5>
-               <img src="img/deneme.jpg" alt="" class="img-fluid">
-               <h5 class="baslikcard">Proje Yönetimi 1</h5>
-               <p class="cardp">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+               <img src="img/bilgisayar.jpg" alt="" class="img-fluid">
+               <h5 class="baslikcard">Banü/Bilgisayar Mühendisliği</h5>
+               <p class="cardp">Bilgisayar ve bilgisayar tabanlı sistemlerin yazılım ve donanımlarının tasarımı, gerçekleştirimi ve yönetimi ile ilgili eğitimler görmektedir.</p>
            </div>
            
            <div class="card item" data-merge=1.5>
-               <img src="img/deneme.jpg" alt="" class="img-fluid">
-               <h5 class="baslikcard">Proje Yönetimi 2</h5>
-               <p class="cardp">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+               <img src="img/yonetim.jpg" alt="" class="img-fluid">
+               <h5 class="baslikcard">Banü/Yönetim Bilişim Sistemleri</h5>
+               <p class="cardp">İnsan, teknoloji, sistem teorisi ve bilgi yönetimi alanlarını bir araya getiren akademik bir eğitim görmektedir.</p>
            </div>
            
               <div class="card item" data-merge=1.5>
-               <img src="img/deneme.jpg" alt="" class="img-fluid">
-               <h5 class="baslikcard">Proje Yönetimi 3</h5>
-               <p class="cardp">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+               <img src="img/veri.jpg" alt="" class="img-fluid">
+               <h5 class="baslikcard">Coursera/Google Proje Yöneticiliğ</h5>
+               <p class="cardp">Google tarafından Temel-Orta-İleri düzeyde 6 alt sertifika eğitimini içeren bir eğitim almaktadır.</p>
            </div>
+           
+           <div class="card item" data-merge=1.5>
+               <img src="img/ai.jpg" alt="" class="img-fluid">
+               <h5 class="baslikcard">Artificial Intelligence and Machine Learning</h5>
+               <p class="cardp">Python ile temelden başlayarak ileri düzeye kadar giden yapay zeka ve makine öğrenimi eğitimini almıştır.</p>
+           </div>
+           
+            <div class="card item" data-merge=1.5>
+               <img src="img/analist.jpg" alt="" class="img-fluid">
+               <h5 class="baslikcard">GMKA/Yapay Zeka Uzmanlığı</h5>
+               <p class="cardp">Python ve Tensorflow ile Yapay Zeka eğitimleri alırken R ve PowerBI ile Veri Bilimi eğitimleri almaktadır.</p>
+           </div>
+           
+            <div class="card item" data-merge=1.5>
+               <img src="img/oua.jpg" alt="" class="img-fluid">
+               <h5 class="baslikcard">OUA/Mobil Uygulama Programcılığı</h5>
+               <p class="cardp">Flutter ve Dart ile mobil programlama eğitiminin yanında "Teknoloji Girişimciliği Eğitimi" ve "2D ve 3D Game Art Eğitimi" gibi kapsayıcı eğitimler almaktadır.</p>
+           </div>
+           
             
            </div>
            
@@ -98,40 +116,40 @@
            <h3 id="faaliyetlerh3">Faaliyetler</h3>
            
            <div class="sutun-sol-sag">
-               <img src="img/denemedort.png" alt="" class="img-fluid oval">
+               <img src="img/dsc.jpg" alt="" class="img-fluid oval">
                <h4 class="faaliyetlerisim">Google DSC</h4>
-               <p class="faal">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+               <p class="faal">Google çatısı altında "Google Teknolojileri" başta olmak üzere her türlü yazılım ve teknolojik organizasyonu yönetme görevini yürüttüm.</p>
                
                <div class="faaliyet-ikon">
-                   <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
-                   <a href="#"><i class="fa-brands fa-instagram social"></i></a>
-                   <a href="#"><i class="fa-brands fa-google social"></i></a>
+                   <a href="https://www.linkedin.com/company/gdsc-banu/"><i class="fa-brands fa-linkedin social"></i></a>
+                   <a href="https://www.instagram.com/gdscbanu/"><i class="fa-brands fa-instagram social"></i></a>
+                   <a href="https://gdsc.community.dev/bandirma-onyedi-eylul-university/"><i class="fa-brands fa-google social"></i></a>
                </div>
                
            </div>
            
            <div class="sutun">
-               <img src="img/denemedort.png" alt="" class="img-fluid oval">
+               <img src="img/set.jpg" alt="" class="img-fluid oval">
                <h4 class="faaliyetlerisim">BANÜ SET</h4>
-               <p class="faal">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+               <p class="faal">Üniversite bünyesinde bulunan en büyük öğrenci topluluğunun her alanda gerçekleştirdiği projeleri oluşturma, yürütme ve kontrol etme görevlerini üstlendim.</p>
                
                <div class="faaliyet-ikon">
-                   <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
-                   <a href="#"><i class="fa-brands fa-instagram social"></i></a>
-                   <a href="#"><i class="fa-brands fa-google social"></i></a>
+                   <a href="https://www.linkedin.com/company/banuset/"><i class="fa-brands fa-linkedin social"></i></a>
+                   <a href="https://www.instagram.com/banu.set/"><i class="fa-brands fa-instagram social"></i></a>
+                   <a href="https://linktr.ee/banu.set"><i class="fa-brands fa-google social"></i></a>
                </div>
                
            </div>
            
            <div class="sutun-sol-sag">
-               <img src="img/denemedort.png" alt="" class="img-fluid oval">
+               <img src="img/akademi.jpg" alt="" class="img-fluid oval">
                <h4 class="faaliyetlerisim">Google Akademisi</h4>
-               <p class="faal">Bu bir deneme yazısıdır. Bu bir deneme yazısıdır. Bu bir deneme yazısıdır.</p>
+               <p class="faal">Mobil Programlama Eğitimi, Google Proje Yönetimi eğitimi, Teknoloji Girişimciliği Eğitimi, 2D ve 3D Game Art Eğitimi aldığım Trainee deneyimine devam ediyorum.</p>
                
                <div class="faaliyet-ikon">
-                   <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
-                   <a href="#"><i class="fa-brands fa-instagram social"></i></a>
-                   <a href="#"><i class="fa-brands fa-google social"></i></a>
+                   <a href="https://www.linkedin.com/company/oyun-ve-uygulama-akademisi/"><i class="fa-brands fa-linkedin social"></i></a>
+                   <a href="https://www.instagram.com/oyunveuygulamaakademisi/"><i class="fa-brands fa-instagram social"></i></a>
+                   <a href="https://www.oyunveuygulamaakademisi.com/"><i class="fa-brands fa-google social"></i></a>
                </div>
                
            </div>
@@ -185,9 +203,9 @@
                
                <div id="socialfooter"> 
                
-                <a href="#"><i class="fa-brands fa-linkedin social"></i></a>
-                <a href="#"><i class="fa-brands fa-instagram social"></i></a>
-                <a href="#"><i class="fa-brands fa-google social"></i></a>
+                <a href="https://www.linkedin.com/in/muhammedakkus/"><i class="fa-brands fa-linkedin social"></i></a>
+                <a href="https://www.instagram.com/mhmdakkus/"><i class="fa-brands fa-instagram social"></i></a>
+                <a href="mailto:muhammedakkus.tr@gmail.com"><i class="fa-brands fa-google social"></i></a>
                 
                 </div>
                 
